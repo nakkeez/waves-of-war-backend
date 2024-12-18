d789eb2fbd14ef46423740d2d058c14cbc0df256
nakkeez <markus.rantaniva@gmail.com>
2024-12-18 23:18:27 +0200

    Create WebSocket server and broadcast received messages to other clients

---
+++ WavesOfWarServer/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WavesOfWarServer.Services;
+
+namespace WavesOfWarServer.Controllers
+{
+    /// <summary>
+    /// Controller to handle WebSocket requests.
+    /// </summary>
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly WebSocketService _webSocketService = new();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomeController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger to log information and errors.</param>
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Handles the WebSocket GET request.
+        /// Listens messages and broadcasts them to all connected clients.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        [HttpGet(Name = "WebSocket")]
+        public async Task Get()
+        {
+            await _webSocketService.HandleWebSocketAsync(HttpContext, _logger);
+        }
+    }
+}
---
+++ WavesOfWarServer/Services/WebSocketService.cs
@@ -0,0 +1,95 @@
+﻿using System.Net.WebSockets;
+
+namespace WavesOfWarServer.Services
+{
+    /// <summary>
+    /// Service to handle WebSocket connections and communication.
+    /// </summary>
+    public class WebSocketService
+    {
+        private static readonly List<WebSocket> _sockets = [];
+
+        /// <summary>
+        /// Handles an incoming WebSocket request.
+        /// If the request is a WebSocket request, it accepts the connection and starts listening for messages.
+        /// If not, it returns a 400 Bad Request status code.
+        /// </summary>
+        /// <param name="context">The HTTP context of the request.</param>
+        /// <param name="logger">The logger to log information and errors.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        public async Task HandleWebSocketAsync(HttpContext context, ILogger logger)
+        {
+
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                using var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                lock (_sockets)
+                {
+                    _sockets.Add(webSocket);
+                }
+                await Echo(webSocket, logger);
+                lock (_sockets)
+                {
+                    _sockets.Remove(webSocket);
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+
+        /// <summary>
+        /// Echoes messages received from the WebSocket back to all connected clients.
+        /// </summary>
+        /// <param name="webSocket">The WebSocket to receive messages from.</param>
+        /// <param name="logger">The logger to log information and errors.</param>
+        /// <returns>A task that represents the asynchronous operation.</returns>
+        private static async Task Echo(WebSocket webSocket, ILogger logger)
+        {
+            var buffer = new byte[1024 * 4];
+            try
+            {
+                var receiveResult = await webSocket.ReceiveAsync(
+                    new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                while (!receiveResult.CloseStatus.HasValue)
+                {
+                    var message = new ArraySegment<byte>(buffer, 0, receiveResult.Count);
+
+                    // Copy the clients to list so lock can be released before broadcasting the messages
+                    List<WebSocket> socketsCopy;
+                    lock (_sockets)
+                    {
+                        socketsCopy = [.. _sockets];
+                    }
+
+                    foreach (WebSocket socket in socketsCopy)
+                    {
+                        if (socket.State == WebSocketState.Open)
+                        {
+                            await socket.SendAsync(
+                                message,
+                                receiveResult.MessageType,
+                                receiveResult.EndOfMessage,
+                                CancellationToken.None);
+                        }
+                    }
+
+                    receiveResult = await webSocket.ReceiveAsync(
+                        new ArraySegment<byte>(buffer), CancellationToken.None);
+                }
+
+                await webSocket.CloseAsync(
+                    receiveResult.CloseStatus.Value,
+                    receiveResult.CloseStatusDescription,
+                    CancellationToken.None);
+
+            }
+            catch (WebSocketException ex)
+            {
+                logger.LogError("WebSocketException: {Message}", ex.Message);
+            }
+        }
+    }
+}
